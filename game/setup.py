c9ebfd8e120584a74ae2b5205d62732a7b187154
Evgeny Namsaraev <enamsaraev13@icloud.com>
2021-04-07 19:43:43 +0300

    game

---
+++ game/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='game',
+    version='1.0.0',
+    description='Shooter',
+    license='MIT',
+    author='Evgeny Namsaraev',
+    author_email='enamsaraev0@icloud.com',
+    packages=find_packages()
+)
